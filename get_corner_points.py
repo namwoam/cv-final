ec59a5693603bf5d29e437d81f129b0f53d5afe0
Blake <a22728033@gmail.com>
2023-06-02 23:48:29 +0800

    remove points on the boundary and camera mask

--- get_corner_points.py
+++ get_corner_points.py
@@ -5,89 +5,130 @@ import os
 import argparse
 from tqdm import tqdm
 
-ZERBRACROSS = 0
+ZEBRACROSS = 0
 STOPLINE = 1
 ARROW = 2
 JUNCTIONBOX = 3
 OTHER = 4
-CLASS_ID_TO_TYPE = ['ZERBRACROSS', 'STOPLINE', 'ARROW', 'JUNCTIONBOX', 'OTHER']
+CLASS_ID_TO_TYPE = ['ZEBRACROSS', 'STOPLINE', 'ARROW', 'JUNCTIONBOX', 'OTHER']
+
+class CornerPointDetector:
+    def __init__(self, data_path, camera_info_path, output_path = None, confidence = 0.2):
+        # ==================================================================================================
+        # data_path (string): path to the folder containing the sequence. ex. public/seq1
+        # camera_info_path (string): path to the camera info directory. ex. public/camera_info/lucid_cameras_x00
+        # output_path (string): path to the folder to save the output. the output will not be saved if this is None
+        # confidence (float): the prediction boxes with confidence lower than this value will be ignored
+        # ==================================================================================================
+
+        self.data_path = data_path
+        self.confidence = confidence
+        self.output_path = output_path
+
+        self.camera_mask = {}
+        self.camera_mask['f'] = cv2.imread(os.path.join(camera_info_path, 'gige_100_f_hdr_mask.png'), cv2.IMREAD_GRAYSCALE)
+        self.camera_mask['b'] = cv2.imread(os.path.join(camera_info_path, 'gige_100_b_hdr_mask.png'), cv2.IMREAD_GRAYSCALE)
+        self.camera_mask['fl'] = cv2.imread(os.path.join(camera_info_path, 'gige_100_fl_hdr_mask.png'), cv2.IMREAD_GRAYSCALE)
+        self.camera_mask['fr'] = cv2.imread(os.path.join(camera_info_path, 'gige_100_fr_hdr_mask.png'), cv2.IMREAD_GRAYSCALE)
+
+        self.camera_mask['f'] = cv2.threshold(self.camera_mask['f'], 127, 255, cv2.THRESH_BINARY)[1]
+        self.camera_mask['b'] = cv2.threshold(self.camera_mask['b'], 127, 255, cv2.THRESH_BINARY)[1]
+        self.camera_mask['fl'] = cv2.threshold(self.camera_mask['fl'], 127, 255, cv2.THRESH_BINARY)[1]
+        self.camera_mask['fr'] = cv2.threshold(self.camera_mask['fr'], 127, 255, cv2.THRESH_BINARY)[1]
+
+        kernel = np.ones((3, 3), np.uint8)
+        self.camera_mask['f'] = cv2.dilate(self.camera_mask['f'], kernel, iterations = 1)
+        self.camera_mask['b'] = cv2.dilate(self.camera_mask['b'], kernel, iterations = 1)
+        self.camera_mask['fl'] = cv2.dilate(self.camera_mask['fl'], kernel, iterations = 1)
+        self.camera_mask['fr'] = cv2.dilate(self.camera_mask['fr'], kernel, iterations = 1)
+
+        self.epsilon = {ZEBRACROSS: 5, STOPLINE: 5, ARROW: 5, JUNCTIONBOX: 5, OTHER: 5}
 
 
-def get_corner_points(data_path, timestamp, confidence = 0.2, output_path = None):
-    # ==================================================================================================
-    # data_path (string): path to the folder containing the sequence. ex. public/seq1
-    # timestamp (string): timestamp of the frame to be processed. ex. 1681710717_532211005
-    # confidence (float): the prediction boxes with confidence lower than this value will be ignored
-    # output_path (string): path to the folder to save the output. the output will not be saved if this is None
-    #
-    # return: a list of corner points (x, y)
-    # ==================================================================================================
+    def get_corner_points(self, timestamp):
+        # ==================================================================================================
+        # timestamp (string): timestamp of the frame to be processed. ex. 1681710717_532211005
+        #
+        # return: a list of corner points (x, y)
+        # ==================================================================================================
 
-    img_path = os.path.join(data_path, 'dataset', timestamp, 'raw_image.jpg')
-    pred_path = os.path.join(data_path, 'dataset', timestamp, 'detect_road_marker.csv')
-    output_path = os.path.join(output_path, timestamp) if output_path else None
+        img_path = os.path.join(self.data_path, 'dataset', timestamp, 'raw_image.jpg')
+        pred_path = os.path.join(self.data_path, 'dataset', timestamp, 'detect_road_marker.csv')
+        output_path = os.path.join(self.output_path, timestamp) if self.output_path else None
 
-    img = cv2.imread(img_path)
-    h, w, _ = img.shape
+        img = cv2.imread(img_path)
+        h, w, _ = img.shape
 
-    pred = pd.read_csv(pred_path, names = ['x1', 'y1', 'x2', 'y2', 'class', 'confidence'])
-    pred = pred[pred['confidence'] >= confidence]
+        pred = pd.read_csv(pred_path, names = ['x1', 'y1', 'x2', 'y2', 'class', 'confidence'])
+        pred = pred[pred['confidence'] >= self.confidence]
 
-    pred[['x1', 'x2']] = np.clip(pred[['x1', 'x2']].astype(int), 0, w - 1)
-    pred[['y1', 'y2']] = np.clip(pred[['y1', 'y2']].astype(int), 0, h - 1)
+        pred[['x1', 'x2']] = np.clip(pred[['x1', 'x2']].astype(int), 0, w - 1)
+        pred[['y1', 'y2']] = np.clip(pred[['y1', 'y2']].astype(int), 0, h - 1)
 
-    if output_path and not os.path.exists(output_path):
-        os.makedirs(output_path)
-    output_img = img.copy()
+        with open(os.path.join(self.data_path, 'dataset', timestamp, 'camera.csv'), 'r') as f:
+            camera_info = f.read()
+        camera_info = camera_info.strip().split('_')[-2]
+        camera_mask = self.camera_mask[camera_info]
 
-    corner_points = []
+        if output_path and not os.path.exists(output_path):
+            os.makedirs(output_path)
+        output_img = img.copy()
 
-    for row in pred.itertuples():
-        index, x1, y1, x2, y2, class_id, confidence = row
-        index += 1
-        class_type = CLASS_ID_TO_TYPE[class_id]
+        corner_points = []
 
-        crop_img = img[y1:y2, x1:x2]
-        crop_gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+        for row in pred.itertuples():
+            index, x1, y1, x2, y2, class_id, confidence = row
+            index += 1
+            class_type = CLASS_ID_TO_TYPE[class_id].lower()
 
-        mean = np.mean(crop_gray)
-        std = np.std(crop_gray)
-        crop_gray = cv2.threshold(crop_gray, mean + std / 2, 255, cv2.THRESH_BINARY)[1]
+            crop_img = img[y1:y2, x1:x2]
+            crop_gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
 
-        if output_path:
-            cv2.imwrite(os.path.join(output_path, f'threshold_{index}_{class_type}.jpg'), crop_gray)
+            mean = np.mean(crop_gray)
+            std = np.std(crop_gray)
+            crop_gray = cv2.threshold(crop_gray, mean + std / 2, 255, cv2.THRESH_BINARY)[1]
 
-        contours = cv2.findContours(crop_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
-
-        if output_path:
-            contour_img = np.zeros((crop_gray.shape[0], crop_gray.shape[1], 3), np.uint8)
-            cv2.drawContours(contour_img, contours, -1, (255, 255, 255), 1)
-            cv2.imwrite(os.path.join(output_path, f'contours_{index}_{class_type}.jpg'), contour_img)
-            approx_img = np.zeros((crop_gray.shape[0], crop_gray.shape[1], 3), np.uint8)
-
-        for contour in contours:
-            approx = cv2.approxPolyDP(contour, 5, True)
             if output_path:
-                cv2.drawContours(approx_img, [approx], -1, (255, 255, 255), 1)
+                cv2.imwrite(os.path.join(output_path, f'threshold_{index}_{class_type}.jpg'), crop_gray)
 
-            for corner in approx:
-                corner_points.append((corner[0][0] + x1, corner[0][1] + y1))
+            contours = cv2.findContours(crop_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+            if output_path:
+                contour_img = np.zeros((crop_gray.shape[0], crop_gray.shape[1], 3), np.uint8)
+                cv2.drawContours(contour_img, contours, -1, (255, 255, 255), 1)
+                cv2.imwrite(os.path.join(output_path, f'contours_{index}_{class_type}.jpg'), contour_img)
+                approx_img = np.zeros((crop_gray.shape[0], crop_gray.shape[1], 3), np.uint8)
+
+            epsilon = self.epsilon[class_id]
+            for contour in contours:
+                approx = cv2.approxPolyDP(contour, epsilon, True)
                 if output_path:
-                    cv2.circle(output_img, (corner[0][0] + x1, corner[0][1] + y1), 2, (0, 0, 255), -1)
+                    cv2.drawContours(approx_img, [approx], -1, (255, 255, 255), 1)
+
+                for corner in approx:
+                    if corner[0][0] + x1 < 7 or corner[0][0] + x1 > w - 7 or corner[0][1] + y1 < 7 or corner[0][1] + y1 > h - 7 or \
+                       camera_mask[corner[0][1] + y1, corner[0][0] + x1] > 0:
+                        continue
+                    corner_points.append((corner[0][0] + x1, corner[0][1] + y1))
+                    if output_path:
+                        cv2.circle(output_img, (corner[0][0] + x1, corner[0][1] + y1), 2, (0, 0, 255), -1)
+
+            if output_path:
+                cv2.imwrite(os.path.join(output_path, f'approx_{index}_{class_type}.jpg'), approx_img)
 
         if output_path:
-            cv2.imwrite(os.path.join(output_path, f'approx_{index}_{class_type}.jpg'), approx_img)
+            cv2.imwrite(os.path.join(output_path, 'output.jpg'), output_img)
 
-    if output_path:
-        cv2.imwrite(os.path.join(output_path, 'output.jpg'), output_img)
-
-    return corner_points
+        return corner_points
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type = str, required = True, help = 'path to the folder containing the sequence')
+    parser.add_argument('--camera_info_path', type = str, required = True, help = 'path to the folder containing the camera info')
     args = parser.parse_args()
+
+    detector = CornerPointDetector(args.data_path, args.camera_info_path, 'outputs')
 
     with open(os.path.join(args.data_path, 'all_timestamp.txt'), 'r') as f:
         timestamps = f.readlines()
@@ -95,7 +136,7 @@ if __name__ == '__main__':
     timestamps = [timestamp.strip() for timestamp in timestamps]
 
     for timestamp in tqdm(timestamps):
-        get_corner_points(args.data_path, timestamp, output_path = 'outputs')
+        detector.get_corner_points(timestamp)
 
 
 
