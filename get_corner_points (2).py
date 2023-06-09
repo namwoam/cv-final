6f8687510f2162722813cf2bf0626b7fe28be848
edison1030905 <128962771+edison1030905@users.noreply.github.com>
2023-06-09 23:59:35 +0800

    Add files via upload

--- get_corner_points (2).py
+++ get_corner_points (2).py
@@ -11,7 +11,11 @@ ARROW = 2
 JUNCTIONBOX = 3
 OTHER = 4
 CLASS_ID_TO_TYPE = ['ZEBRACROSS', 'STOPLINE', 'ARROW', 'JUNCTIONBOX', 'OTHER']
-MIN_AREA_RATIO = 1/200
+POINT_COLOR = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 0), (0, 255, 255)]
+SHORTEST_DISTANCE_TO_BORDER = 2
+SHORTEST_DISTNACE_TO_POINT = 4
+AREA_THRESHOLD = 10
+EPSILON = 4
 
 class CornerPointDetector:
     def __init__(self, data_path, camera_info_path, output_path = None, confidence = 0.2):
@@ -37,14 +41,11 @@ class CornerPointDetector:
         self.camera_mask['fl'] = cv2.threshold(self.camera_mask['fl'], 127, 255, cv2.THRESH_BINARY)[1]
         self.camera_mask['fr'] = cv2.threshold(self.camera_mask['fr'], 127, 255, cv2.THRESH_BINARY)[1]
 
-        kernel = np.ones((3, 3), np.uint8)
+        kernel = np.ones((9, 9), np.uint8)
         self.camera_mask['f'] = cv2.dilate(self.camera_mask['f'], kernel, iterations = 1)
         self.camera_mask['b'] = cv2.dilate(self.camera_mask['b'], kernel, iterations = 1)
         self.camera_mask['fl'] = cv2.dilate(self.camera_mask['fl'], kernel, iterations = 1)
         self.camera_mask['fr'] = cv2.dilate(self.camera_mask['fr'], kernel, iterations = 1)
-
-        self.epsilon = {ZEBRACROSS: 7, STOPLINE: 10, ARROW: 6, JUNCTIONBOX: 5, OTHER: 10}
-
 
     def get_corner_points(self, timestamp):
         # ==================================================================================================
@@ -73,8 +74,12 @@ class CornerPointDetector:
 
         if output_path and not os.path.exists(output_path):
             os.makedirs(output_path)
-        output_img = img.copy()
 
+        if output_path:
+            output_img = img.copy()
+            box_img = img.copy()
+
+        point_mask = np.zeros((h, w), dtype = np.uint8)
         corner_points = []
 
         for row in pred.itertuples():
@@ -82,82 +87,94 @@ class CornerPointDetector:
             index += 1
             class_type = CLASS_ID_TO_TYPE[class_id].lower()
 
-            crop_img = img[y1:y2, x1:x2]
+            # Save box image
+            if output_path:
+                cv2.rectangle(box_img, (x1, y1), (x2, y2), POINT_COLOR[class_id], 2)
+                cv2.putText(box_img, f'{index}_{class_type}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, POINT_COLOR[class_id], 2)
 
-            # crop_gray = cv2.cvtColor(crop_img,cv2.COLOR_BGR2GRAY)
+            crop_img = img[y1:y2, x1:x2]
+            # crop_gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+
+            ###################################################
+            #Modify grayscale
+            ####################################################
             B,G,R = crop_img[:,:,0],crop_img[:,:,1],crop_img[:,:,2]
             a = np.array([3,-1,-1]).astype(np.float32)
             a /= np.sum(a)
             crop_gray = a[0]*B +a[1]*G+a[2]*R
             crop_gray = crop_gray.astype(np.uint8)
-            cv2.imwrite('gray.png',crop_gray)
+            #############################################
 
             mean = np.mean(crop_gray)
             std = np.std(crop_gray)
-            crop_gray = cv2.threshold(crop_gray, mean + std / 2, 255, cv2.THRESH_BINARY)[1]
+            crop_gray = cv2.threshold(crop_gray, mean + std * 2 / 3 , 255, cv2.THRESH_BINARY)[1]
             h_bar,w_bar = crop_gray.shape
-            #####################################################################################
+            ################################################################################
+            #erode and dilate
+            ################################################################################
 
-            # white = np.sum(crop_gray) //255
-            # total = h_bar*w_bar
-            # if class_id == 0 and white < total/2.5:
-            # # if True:
-            #     kernal = np.ones((6,6),np.uint8)
-            #     crop_gray = cv2.dilate(crop_gray,kernal,iterations=1)
-            #     crop_gray = cv2.erode(crop_gray,kernal,iterations=1)
-            #     kernal2 = np.ones((2,2),np.uint8)
-            #     crop_gray = cv2.erode(crop_gray,kernal2,iterations=1)
-            #     crop_gray = cv2.dilate(crop_gray,kernal2,iterations=1)
-
-            ######################################################################################
-
+            white = np.sum(crop_gray) //255
+            total = h_bar*w_bar
+            if class_id == 0 and white < total/2.5:
+                kernal = np.ones((6,6),np.uint8)
+                crop_gray = cv2.dilate(crop_gray,kernal,iterations=1)
+                crop_gray = cv2.erode(crop_gray,kernal,iterations=1)
+                kernal2 = np.ones((2,2),np.uint8)
+                crop_gray = cv2.erode(crop_gray,kernal2,iterations=1)
+                crop_gray = cv2.dilate(crop_gray,kernal2,iterations=1)
+            ########################################################################
+            #delete small point
+            ########################################################################
+            MIN_AREA_RATIO = 1/200
             contours = cv2.findContours(crop_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
             threshold = (h_bar)*(w_bar)*MIN_AREA_RATIO
             for i in range(len(contours)):
                 area = cv2.contourArea(contours[i])
                 if area < threshold:
                     cv2.drawContours(crop_gray,[contours[i]],-1,0,thickness=-1)
-
-
-            #######################################################################################
-
-
-
-
-
-
-            ######################################################################################
+            #########################################################################
+            # Save threshold image
             if output_path:
                 cv2.imwrite(os.path.join(output_path, f'threshold_{index}_{class_type}.jpg'), crop_gray)
 
-
             contours = cv2.findContours(crop_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
 
+            # Save contour image
             if output_path:
                 contour_img = np.zeros((crop_gray.shape[0], crop_gray.shape[1], 3), np.uint8)
                 cv2.drawContours(contour_img, contours, -1, (255, 255, 255), 1)
                 cv2.imwrite(os.path.join(output_path, f'contours_{index}_{class_type}.jpg'), contour_img)
                 approx_img = np.zeros((crop_gray.shape[0], crop_gray.shape[1], 3), np.uint8)
 
-            epsilon = self.epsilon[class_id]
             for contour in contours:
-                approx = cv2.approxPolyDP(contour, epsilon, True)
+                if cv2.contourArea(contour) < AREA_THRESHOLD:
+                    continue
+
+                approx = cv2.approxPolyDP(contour, EPSILON, True)
                 if output_path:
                     cv2.drawContours(approx_img, [approx], -1, (255, 255, 255), 1)
 
                 for corner in approx:
-                    if corner[0][0] + x1 < 7 or corner[0][0] + x1 > w - 7 or corner[0][1] + y1 < 7 or corner[0][1] + y1 > h - 7 or \
-                       camera_mask[corner[0][1] + y1, corner[0][0] + x1] > 0:
+                    actual_x, actual_y = corner[0][0] + x1, corner[0][1] + y1
+                    if corner[0][0] < SHORTEST_DISTANCE_TO_BORDER or corner[0][0] > crop_img.shape[1] - SHORTEST_DISTANCE_TO_BORDER or corner[0][1] < SHORTEST_DISTANCE_TO_BORDER or corner[0][1] > crop_img.shape[0] - SHORTEST_DISTANCE_TO_BORDER or \
+                       camera_mask[actual_y, actual_x] > 0 or \
+                       point_mask[actual_y, actual_x] > 0:
                         continue
                     corner_points.append((corner[0][0] + x1, corner[0][1] + y1))
+                    point_mask[actual_y - SHORTEST_DISTNACE_TO_POINT:actual_y + SHORTEST_DISTNACE_TO_POINT, actual_x - SHORTEST_DISTNACE_TO_POINT:actual_x + SHORTEST_DISTNACE_TO_POINT] = 255
                     if output_path:
-                        cv2.circle(output_img, (corner[0][0] + x1, corner[0][1] + y1), 2, (0, 0, 255), -1)
+                        cv2.circle(output_img, (corner[0][0] + x1, corner[0][1] + y1), 2, POINT_COLOR[class_id], -1)
 
+            # Save approx image
             if output_path:
                 cv2.imwrite(os.path.join(output_path, f'approx_{index}_{class_type}.jpg'), approx_img)
-        # output_img[corner_points] =
+
+
         if output_path:
+            # Save image with corner points
             cv2.imwrite(os.path.join(output_path, 'output.jpg'), output_img)
+            # Save image with boxes
+            cv2.imwrite(os.path.join(output_path, 'box.jpg'), box_img)
 
         return corner_points
 
@@ -167,8 +184,8 @@ if __name__ == '__main__':
     parser.add_argument('--data_path', type = str, required = True, help = 'path to the folder containing the sequence')
     parser.add_argument('--camera_info_path', type = str, required = True, help = 'path to the folder containing the camera info')
     args = parser.parse_args()
-    out = os.path.join(args.data_path,'output')
-    detector = CornerPointDetector(args.data_path, args.camera_info_path, out)
+
+    detector = CornerPointDetector(args.data_path, args.camera_info_path, 'outputs')
 
     with open(os.path.join(args.data_path, 'all_timestamp.txt'), 'r') as f:
         timestamps = f.readlines()
